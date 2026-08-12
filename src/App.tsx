3d806d1037ddcec5036c58798ddd50b14b95d10d
ganpm <prmgandia@gmail.com>
2026-08-13 06:50:38 +0800

    refactor: remove OperationsView, ProductionView, and PowerGridView, update to use ProductionOverview and AnalyticsOverview

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,8 @@ import {
   TabsContent,
 } from "@/components/ui/tabs";
 
-import { OperationsView } from "@/features/operations-view";
-import { ProductionView } from "@/features/production-view";
-import { PowerGridView } from "@/features/power-grid-view";
+import { ProductionOverview } from "@/features/production.tsx";
+import { AnalyticsOverview } from "@/features/analytics.tsx";
 
 
 export const App = () => {
@@ -18,22 +17,18 @@ export const App = () => {
     <div className="md:max-w-lg w-full h-screen mx-auto">
       <Tabs defaultValue="operations">
         <div className="relative flex-1 pb-16">
-          <TabsContent value="operations">
-            <OperationsView game={game} snapshot={snapshot} />
-          </TabsContent>
-
           <TabsContent value="production">
-            <ProductionView chartData={chartData} />
+            <ProductionOverview game={game} snapshot={snapshot} />
           </TabsContent>
 
-          <TabsContent value="power-grid">
-            <PowerGridView />
+          <TabsContent value="analytics">
+            <AnalyticsOverview chartData={chartData} />
           </TabsContent>
+
         </div>
         <TabsList className="fixed bottom-0 z-50 md:max-w-lg w-full justify-center" variant="line">
-          <TabsTrigger value="operations">Operations</TabsTrigger>
           <TabsTrigger value="production">Production</TabsTrigger>
-          <TabsTrigger value="power-grid">Power Grid</TabsTrigger>
+          <TabsTrigger value="analytics">Analytics</TabsTrigger>
         </TabsList>
       </Tabs>
     </div>
