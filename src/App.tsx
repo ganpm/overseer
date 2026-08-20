9c0c5ce71d328a597a58f6067a3ed6ef8effdc1b
ganpm <prmgandia@gmail.com>
2026-08-20 15:32:21 +0800

    refactor: adjust layout width for App component and tabs

--- src/App.tsx
+++ src/App.tsx
@@ -14,19 +14,17 @@ export const App = () => {
   const { game, snapshot, chartData } = useGame();
 
   return (
-    <div className="md:max-w-lg w-full h-screen mx-auto">
+    <div className="md:w-md w-full h-screen mx-auto">
       <Tabs defaultValue="operations">
         <div className="relative flex-1 pb-16">
           <TabsContent value="production">
             <ProductionOverview game={game} snapshot={snapshot} />
           </TabsContent>
-
           <TabsContent value="analytics">
             <AnalyticsOverview chartData={chartData} />
           </TabsContent>
-
         </div>
-        <TabsList className="fixed bottom-0 z-50 md:max-w-lg w-full justify-center" variant="line">
+        <TabsList className="fixed bottom-0 z-50 md:w-md justify-center" variant="line">
           <TabsTrigger value="production">Production</TabsTrigger>
           <TabsTrigger value="analytics">Analytics</TabsTrigger>
         </TabsList>
