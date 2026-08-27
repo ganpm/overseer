66a02c4d9dc2c2dfd8e1dc4cf3df3fcb2ca05088
ganpm <prmgandia@gmail.com>
2026-08-27 17:34:56 +0800

    fix: fix overflow issue

--- src/App.tsx
+++ src/App.tsx
@@ -14,15 +14,15 @@ export const App = () => {
   const { game, snapshot, throughputChartData, powerChartData } = useGame();
 
   return (
-    <div className="flex flex-col h-screen w-full bg-foreground overflow-y-auto scrollbar-hidden">
-      <Tabs defaultValue="production" className="flex-1 mx-auto w-full md:w-md bg-background">
-        <TabsContent value="production">
+    <div className="flex flex-col h-screen w-full bg-foreground overflow-hidden">
+      <Tabs defaultValue="production" className="mx-auto flex-1 min-h-0 w-full md:w-md bg-background">
+        <TabsContent value="production" className="min-h-0 overflow-auto scrollbar-hidden">
           <ProductionOverview game={game} snapshot={snapshot} />
         </TabsContent>
-        <TabsContent value="analytics">
+        <TabsContent value="analytics" className="min-h-0 overflow-auto scrollbar-hidden">
           <AnalyticsOverview throughputChartData={throughputChartData} powerChartData={powerChartData} />
         </TabsContent>
-        <TabsList className="fixed bottom-0 z-50 md:w-md w-full justify-center" variant="line">
+        <TabsList className="md:w-md w-full justify-center" variant="line">
           <TabsTrigger value="production">Production</TabsTrigger>
           <TabsTrigger value="analytics">Analytics</TabsTrigger>
         </TabsList>
