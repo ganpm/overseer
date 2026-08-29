53153207ea267a049d93a0092915001662cb3715
ganpm <prmgandia@gmail.com>
2026-08-29 14:13:12 +0800

    feat: add play/pause button in MenuBar component

--- src/components/menu-bar.tsx
+++ src/components/menu-bar.tsx
@@ -1,4 +1,10 @@
 import { cn } from "@/lib/utils";
+import {
+  Pause as PauseIcon,
+  Play as PlayIcon,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export interface MenuBarProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string;
@@ -9,16 +15,29 @@ export function MenuBar({
   className,
   ...props
 }: MenuBarProps) {
+  const [paused, setPaused] = useState(false);
   return (
     <div
       className={cn([
         "text-foreground text-base font-medium",
-        "flex",
+        "flex items-center",
         className
       ])}
       {...props}
     >
-      {title}
+      <span className="flex-1 flex items-center justify-start">{title}</span>
+      <div className="flex-1 flex items-center justify-end">
+        <span className="flex items-center gap-2 text-sm text-muted-foreground">
+          {paused && ("Paused")}
+          <Button variant="outline" onClick={() => setPaused((prev) => !prev)}>
+            {paused ? (
+              <PauseIcon fill="#000" stroke="#000" />
+            ) : (
+              <PlayIcon fill="#000" stroke="#000" />
+            )}
+          </Button>
+        </span>
+      </div>
     </div>
   );
 }
