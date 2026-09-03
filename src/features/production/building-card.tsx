13561a490b9741baf9bd56673695634f5264ece9
ganpm <prmgandia@gmail.com>
2026-09-03 17:07:16 +0800

    feat: enhance BuildingCard UI

--- src/features/production/building-card.tsx
+++ src/features/production/building-card.tsx
@@ -1,13 +1,53 @@
+import { cn } from "@/lib/utils";
 import type { BuildingGroupInstance } from "pkg/overseer";
 import { Button } from "@/components/ui/button";
 import {
   Plus as PlusIcon,
   Minus as MinusIcon,
-  Power as PowerIcon,
+  CircleSmall as StatusIcon,
+  PowerOff as DisabledIcon,
+  Zap as PowerIcon,
+  ZapOff as UnpoweredIcon,
+  TriangleAlert as AlertIcon,
+  ChevronUp as UpIcon,
+  ChevronDown as DownIcon,
 } from "lucide-react";
 import { ProgressBar } from "@/components/progress-bar";
 import { ProcessCard } from "@/features/production/process-card";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
 
+const status = {
+  online: "var(--color-green-500)", // #22C55E
+  offline: "var(--color-red-500)", // #EF4444
+  warning: "var(--color-yellow-500)", // #F59E0B
+  disabled: "var(--color-gray-400)", // #9CA3AF
+};
+
+const getStatusColor = (active: boolean, efficient: boolean, enabled: boolean) => {
+  if (!enabled) return status.disabled;
+  if (!active) return status.offline;
+  if (!efficient) return status.warning;
+  return status.online;
+};
+
+const powerFmt = (number: number) => number.toLocaleString(undefined, {
+  minimumFractionDigits: 1,
+  maximumFractionDigits: 2,
+  signDisplay: "never",
+})
+
+const speedFmt = (number: number) => number.toLocaleString(undefined, {
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 2,
+  signDisplay: "never",
+})
+
+const getSpeedColor = (efficiency:number): { fg: string, bg: string } => {
+  if (efficiency >= 1.0) return { fg: "text-green-800", bg: "bg-green-100" };
+  if (efficiency > 0.0) return { fg: "text-yellow-800", bg: "bg-yellow-100" };
+  return { fg: "text-muted-foreground", bg: "bg-muted" };
+};
 
 export interface BuildingCardProps extends React.HTMLAttributes<HTMLDivElement> {
   buildingGroup: BuildingGroupInstance;
@@ -23,31 +63,34 @@ export function BuildingCard({
   setEnabled,
   ...props
 }: BuildingCardProps) {
-  const { name, totalCount, activeCount, idleCount, process, enabled } = buildingGroup;
+  const { name, totalCount, activeCount, process, enabled } = buildingGroup;
+  const isActive = activeCount > 0;
+  const isPowered = process.efficiency > 0.0;
+  const isEfficient = process.efficiency >= 1.0;
+  const isEnabled = enabled;
+  const statusColor = getStatusColor(isActive, isEfficient, isEnabled);
+  const noInput = isPowered && isEnabled && !isActive;
+
+  const netActivePower = (process.powerGeneration - process.powerConsumption) * activeCount;
+  const netTotalPower = (process.powerGeneration - process.powerConsumption) * totalCount;
+
+  const noPower = process.efficiency === 0.0 && isEnabled;
+  const lowPower = process.efficiency > 0.0 && process.efficiency < 1.0 && isEnabled;
+
+  const speed = process.efficiency * 100;
+  const speedColor = getSpeedColor(process.efficiency);
+
   return (
-    <div className="flex flex-col gap-3 border rounded-md p-3 w-full" {...props}>
-      <div className="flex">
-        <div className="flex-1 flex flex-col gap-1">
-          <span>
-            <span className="font-medium">{name} &times; {totalCount}</span>
-            <span className="text-xs text-muted-foreground ml-2">
-              ({activeCount} active, {idleCount} idle)
-            </span>
-          </span>
-          <span className="font-medium text-xs text-muted-foreground">
-            {process.name}
-          </span>
+    <div className="flex flex-col gap-2 border rounded-md p-3 w-full bg-card" {...props}>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-1">
+          <StatusIcon size="14" fill={statusColor} stroke={statusColor} />
+          <span className="font-medium">{name}</span>
         </div>
-        <div className="flex gap-1">
-          <Button variant="outline" size="sm" onClick={setEnabled}>
-            <PowerIcon size={16} color={enabled ? "#22C55E" : "#9CA3AF"} />
-          </Button>
-          <Button variant="outline" size="sm" onClick={increaseCount}>
-            <PlusIcon size={16} />
-          </Button>
-          <Button variant="destructive" size="sm" onClick={decreaseCount}>
-            <MinusIcon size={16} />
-          </Button>
+        <div className="flex items-center">
+          <span className="text-xs text-muted-foreground">
+            {activeCount} / {totalCount} Active
+          </span>
         </div>
       </div>
       <ProgressBar
@@ -56,6 +99,88 @@ export function BuildingCard({
         active={activeCount > 0 && buildingGroup.enabled}
       />
       <ProcessCard process={process} />
+      <div className="flex items-center justify-between">
+        {netTotalPower !== 0.0 && (
+          <div className="flex items-center gap-1 px-2 py-0.5 rounded-md bg-secondary">
+            <span className="flex gap-0">
+              {netActivePower >= 0 ? <UpIcon size={10} /> : <DownIcon size={10} />}
+            </span>
+            <span className="font-medium">
+              {powerFmt(netActivePower)} MW
+            </span>
+            <span className="text-muted-foreground">/</span>
+            <span className="text-muted-foreground">
+              {powerFmt(netTotalPower)} MW
+            </span>
+          </div>
+        )}
+        <div>
+          <span
+            className={cn(
+              "text-muted-foreground w-10 px-2 py-0.5 rounded-md",
+              speedColor.bg,
+              speedColor.fg,
+            )}
+          >
+            {speedFmt(speed)}% speed
+          </span>
+        </div>
+      </div>
+      <div className="flex min-h-5 gap-2 items-center">
+        {!enabled && (
+          <div className="flex items-center gap-1">
+            <DisabledIcon size={13} stroke={status.disabled} />
+            <span className="text-muted-foreground">Offline</span>
+          </div>
+        )}
+        {lowPower && (
+          <div className="flex items-center gap-1">
+            <PowerIcon size={13} stroke={status.warning} />
+            <span className="text-muted-foreground">Low power</span>
+          </div>
+        )}
+        {noPower && (
+          <div className="flex items-center gap-1">
+            <UnpoweredIcon size={13} stroke={status.offline} />
+            <span className="text-muted-foreground">No power</span>
+          </div>
+        )}
+        {noInput && (
+          <div className="flex items-center gap-1">
+            <AlertIcon size={13} stroke={status.offline} />
+            <span className="text-muted-foreground">No input</span>
+          </div>
+        )}
+      </div>
+      <Separator />
+      <div className="flex justify-between" >
+        <div className="flex items-center gap-2">
+          <Switch
+            checked={isEnabled}
+            onCheckedChange={setEnabled}
+            id={`switch-${name}-${process.name}`}
+            className="cursor-pointer data-checked:bg-green-500"
+          />
+          <label
+            htmlFor={`switch-${name}-${process.name}`}
+            className={cn(
+              "text-sm cursor-pointer select-none",
+              isEnabled ? "" : "text-muted-foreground"
+            )}
+          >
+            {isEnabled ? "On" : "Off"}
+          </label>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button variant="outline" size="sm" onClick={decreaseCount}>
+            <MinusIcon size={16} />
+          </Button>
+          <span className="text-xs">{totalCount}</span>
+          <Button variant="outline" size="sm" onClick={increaseCount}>
+            <PlusIcon size={16} />  
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
