af1841df29517fdc46ea1cb9f17c93b96e126394
ganpm <prmgandia@gmail.com>
2026-09-03 17:07:29 +0800

    feat: enhance ProcessCard UI

--- src/features/production/process-card.tsx
+++ src/features/production/process-card.tsx
@@ -4,7 +4,7 @@ import {
   ArrowBigRight as ProcessIcon,
   Infinity as InfinityIcon,
   Zap as PowerIcon,
-  Timer as DurationIcon,
+  Clock as DurationIcon,
   SquareArrowRightEnter as InputIcon,
   SquareArrowRightExit as OutputIcon,
 } from "lucide-react"
@@ -17,18 +17,18 @@ export function ProcessCard({
   process
 }: ProcessCardProps) {
   return (
-    <div className="flex gap-1 w-full text-muted-foreground">
-      <div className="flex-1 flex flex-col justify-start items-start overflow-x-auto">
+    <div className="flex w-full text-muted-foreground border rounded-md">
+      <div className="flex-1 flex flex-col justify-start items-start p-2 overflow-x-auto">
         {process.inputs.length > 0 && (
           process.inputs.map((input) => (
             (input.amount > 0) ? (
               <div key={input.resource} className="flex items-center gap-1 whitespace-nowrap">
-                <InputIcon size={16} />
+                <InputIcon size={13} />
                 {input.amount} {input.resource}
               </div>
             ) : (
               <div key={input.resource} className="flex items-center gap-1 whitespace-nowrap">
-                <InfinityIcon size={16} />
+                <InfinityIcon size={13} />
                 {input.resource}
               </div>
             )
@@ -36,37 +36,37 @@ export function ProcessCard({
         )}
         {process.powerConsumption > 0 && (
           <div className="flex items-center gap-1">
-            <PowerIcon size={16} />
+            <PowerIcon size={13} />
             {process.powerConsumption} MW
           </div>
         )}
       </div>
-      <Separator orientation="vertical" />
-      <div className="flex flex-col justify-start items-center w-12">
+      <Separator orientation="vertical" className="my-2" />
+      <div className="flex flex-col justify-start items-center p-2 w-fit">
         <div className="flex items-center select-none">
           &nbsp;
-          <ProcessIcon size={16} />
+          <ProcessIcon size={13} />
           &nbsp;
         </div>
         {process.duration > 0 && (
-          <div className="flex items-center">
-            <DurationIcon size={16} />
+          <div className="flex items-center gap-1">
+            <DurationIcon size={13} />
             {process.duration.toFixed(1)}s
           </div>
         )}
       </div>
-      <Separator orientation="vertical" />
-      <div className="flex-1 flex flex-col justify-start items-start overflow-x-auto">
+      <Separator orientation="vertical" className="my-2" />
+      <div className="flex-1 flex flex-col justify-start text-foreground items-start p-2 overflow-x-auto">
         {process.outputs.length > 0 && (
           process.outputs.map((output) => (
             (output.amount > 0) ? (
               <div key={output.resource} className="flex items-center gap-1 whitespace-nowrap">
-                <OutputIcon size={16} />
+                <OutputIcon size={13} />
                 {output.amount} {output.resource}
               </div>
             ) : (
               <div key={output.resource} className="flex items-center gap-1 whitespace-nowrap">
-                <InfinityIcon size={16} />
+                <InfinityIcon size={13} />
                 {output.resource}
               </div>
             )
@@ -74,7 +74,7 @@ export function ProcessCard({
         )}
         {process.powerGeneration > 0 && (
           <div className="flex items-center gap-1">
-            <PowerIcon size={16} />
+            <PowerIcon size={13} />
             {process.powerGeneration} MW
           </div>
         )}
